fdf68ceef99ca17274cb80d19d4edfba8a0ff556
RubesWorld <hi@rubesworld.live>
2021-02-27 22:01:33 -0800

    Updated Database

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -5,32 +5,30 @@ exports.up = async (knex) => {
       users.string("username", 200).notNullable();
       users.string("password", 200).notNullable();
       users.integer("phoneNumber", 320).notNullable();
-      users.timestamps(false, true);
+      users.timestamps(true, true);
     })
     .createTable("plants", (plants) => {
       plants.increments("plant_id");
       plants.string("nickname").notNullable().unique();
       plants.string("species").notNullable();
       plants.string("frequency").notNullable();
-      plants 
-        .integer('species_id')
+      plants
+        .integer("species_id")
         .unsigned()
         .notNullable()
-        .references('plant_id')
-        .inTable('plants')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
+        .references("plant_id")
+        .inTable("plants")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      plants.timestamps(true, true);
     })
     .createTable("species", (species) => {
       species.increments("species_id");
-      species.string('species_name').notNullable().unique();
-      species.
-    });
-    .createTable("plants", (plants) => {
-      plants.increments("plant_id");
-      plants.string("nickname").notNullable().unique();
-      plants.string("species").notNullable();
-      plants.string("frequency").notNullable();
+      species.string("species_name");
+    })
+    .createTable("intervals", (interval) => {
+      interval.increments("interval_id");
+      interval.string("interval_type_name");
     });
 };
 
