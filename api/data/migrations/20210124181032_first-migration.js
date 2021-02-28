b0e7328848bf0e9a01954b265c53209ffb01f3d8
RubesWorld <hi@rubesworld.live>
2021-02-27 18:54:15 -0800

    database work

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,18 +1,37 @@
 exports.up = async (knex) => {
-  await knex.schema.createTable("users", (users) => {
-    users.increments("user_id");
-    users.string("username", 200).notNullable();
-    users.string("password", 200).notNullable();
-    users.integer("phoneNumber", 320).notNullable();
-    users.timestamps(false, true);
-
-    .createTable('plants', (plants) => {
-      plants.increments('plant_id');
-      plants.string('nickname').notNullable().unique()
-      plants.string('species').notNullable()
-      plants.string('h20Frequency').notNullable()
+  await knex.schema
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable();
+      users.string("password", 200).notNullable();
+      users.integer("phoneNumber", 320).notNullable();
+      users.timestamps(false, true);
     })
-  });
+    .createTable("plants", (plants) => {
+      plants.increments("plant_id");
+      plants.string("nickname").notNullable().unique();
+      plants.string("species").notNullable();
+      plants.string("frequency").notNullable();
+      plants 
+        .integer('species_id')
+        .unsigned()
+        .notNullable()
+        .references('plant_id')
+        .inTable('plants')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+    .createTable("species", (species) => {
+      species.increments("species_id");
+      species.string('species_name').notNullable().unique();
+      species.
+    });
+    .createTable("plants", (plants) => {
+      plants.increments("plant_id");
+      plants.string("nickname").notNullable().unique();
+      plants.string("species").notNullable();
+      plants.string("frequency").notNullable();
+    });
 };
 
 exports.down = async (knex) => {
